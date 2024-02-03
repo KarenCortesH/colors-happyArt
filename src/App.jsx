9be1d716d7d3b5df77b7428c728051bb77a477e8
Karen Cortes <karencortes9828@gmail.com>
2024-02-03 18:21:13 -0500

    feat : commit initial

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import Color from './components/Color'
+
+export default function App() {
+  return (
+    <Color></Color>
+  )
+}
---
+++ src/components/Color.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Color = () => {
+    return (
+        <div class="bg-white">
+            <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <h1 class="text-2xl font-bold tracking-tight text-gray-900">Color</h1>
+            </div>
+        </div>
+
+    )
+}
+export default Color
