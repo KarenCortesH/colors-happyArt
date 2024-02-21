8b943f88ed701b1963190361fc97ca81e8de6e5b
Karen Cortes <karencortes9828@gmail.com>
2024-02-21 00:06:46 -0500

    feat:create de Person component , and customize  save Color

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-export const Person = () => {
+const Person = ({ numberOfPeople, setNumberOfPeople }) => {
+    const handleNumberOfPeopleChange = (e) => {
+        const value = parseInt(e.target.value);
+        setNumberOfPeople(value);
+    };
+
     return (
-        <div>Person</div>
-    )
-}
+        <div>
+            <div className="text-center font-bold text-xl mb-4">Ingresa el número de personas:</div>
+            <input
+                type="number"
+                value={numberOfPeople}
+                onChange={handleNumberOfPeopleChange}
+                className="block w-full p-2 mb-4"
+            />
+        </div>
+    );
+};
+
+export default Person;
