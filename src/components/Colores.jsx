4a2e84eb0c800b4063c424e02a8b2f7f60e8f537
Karen Cortes <karencortes9828@gmail.com>
2024-03-12 00:19:14 -0500

    feat: update restructuring of view

--- src/components/Colores.jsx
+++ src/components/Colores.jsx
@@ -6,25 +6,30 @@ const Colores = () => {
     const [selectedColors, setSelectedColors] = useState({});
     const [selectedPerson, setSelectedPerson] = useState('');
     const [numberOfPeople, setNumberOfPeople] = useState(0);
+    const [selectedTable, setSelectedTable] = useState('');
     const navigate = useNavigate();
 
     const handleColorSelect = (color) => {
-        if (!selectedPerson) {
-            alert('Por favor selecciona una persona.');
+        if (!selectedPerson || !selectedTable) {
+            alert('Por favor selecciona una persona y una mesa.');
             return;
         }
 
-        const personColors = selectedColors[selectedPerson] || [];
+        const personColors = selectedColors[selectedTable] || {};
 
-        if (personColors.includes(color)) {
+        if (personColors[selectedPerson]?.includes(color)) {
+            const updatedPersonColors = { ...personColors };
+            updatedPersonColors[selectedPerson] = updatedPersonColors[selectedPerson].filter((c) => c !== color);
             setSelectedColors({
                 ...selectedColors,
-                [selectedPerson]: personColors.filter((c) => c !== color),
+                [selectedTable]: updatedPersonColors,
             });
         } else {
+            const updatedPersonColors = { ...personColors };
+            updatedPersonColors[selectedPerson] = [...(updatedPersonColors[selectedPerson] || []), color];
             setSelectedColors({
                 ...selectedColors,
-                [selectedPerson]: [...personColors, color],
+                [selectedTable]: updatedPersonColors,
             });
         }
     };
@@ -33,7 +38,6 @@ const Colores = () => {
         localStorage.setItem('selectedColors', JSON.stringify(selectedColors));
         alert('Colores Guardados');
         navigate('/OtraPagina');
-
     };
 
     useEffect(() => {
@@ -45,6 +49,10 @@ const Colores = () => {
         setNumberOfPeople(value);
     };
 
+    const handleTableChange = (e) => {
+        setSelectedTable(e.target.value);
+    };
+
     const people = Array.from({ length: numberOfPeople }, (_, index) => `Persona ${index + 1}`);
 
     return (
@@ -54,41 +62,54 @@ const Colores = () => {
                     <img src="../assets/Logo.jpeg" alt="Logo" className="w-50 h-32 mx-auto mb-4" />
                 </div>
 
-                <div className="text-center font text-lg mb-4">Número de personas</div>
-                <input
-                    type="number"
-                    value={numberOfPeople}
-                    onChange={handleNumberOfPeopleChange}
-                    className="block w-full p-2 mb-4"
-                />
-                <div className="text-center font text-lg mb-4">Selecciona cada persona:</div>
-                <select
-                    value={selectedPerson}
-                    onChange={(e) => setSelectedPerson(e.target.value)}
-                    className="block  italic w-full p-2 mb-4"
-                >
-                    <option value="">Selecciona una persona...</option>
-                    {people.map((person, index) => (
-                        <option key={index} value={person}>{person}</option>
-                    ))}
-                </select>
-                <div className="text-center font  text-lg mb-4">Elige los colores que necesitas</div>
-                <br />
+                <div className="flex justify-between mb-4">
+                    <div className="w-1/3">
+                        <select
+                            value={selectedTable}
+                            onChange={handleTableChange}
+                            className="block p-2"
+                        >
+                            <option value="">Selecciona una mesa</option>
+                            <option value="Mesa1">Mesa 1</option>
+                            <option value="Mesa2">Mesa 2</option>
+                        </select>
+                    </div>
+                    <div className="w-1/3">
+                        <input
+                            type="number"
+                            value={numberOfPeople}
+                            onChange={handleNumberOfPeopleChange}
+                            className="block w-full p-2"
+                            placeholder="Ingresa el numero de personas"
+                        />
+                    </div>
+                    <div className="w-1/3">
+                        <select
+                            value={selectedPerson}
+                            onChange={(e) => setSelectedPerson(e.target.value)}
+                            className="block italic w-full p-2"
+                        >
+                            <option value="">Selecciona una persona</option>
+                            {people.map((person, index) => (
+                                <option key={index} value={person}>{person}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+
+                <div className="text-center font-bold text-lg mb-4">Elige los colores que necesitas</div>
                 <div className="grid grid-cols-5 gap-4 justify-items-center">
                     {Array.from({ length: 20 }).map((_, i) => (
                         <img
                             key={i}
                             src={`../assets/Color${i + 1}.jpg`}
                             alt={`Color-${i}`}
-                            className={`w-32 h-32 cursor-pointer ${selectedColors[selectedPerson] && selectedColors[selectedPerson].includes(`Color${i + 1}`) ? 'border-blue-500' : 'border-gray-300'
+                            className={`w-32 h-32 cursor-pointer ${selectedColors[selectedTable] && selectedColors[selectedTable][selectedPerson] && selectedColors[selectedTable][selectedPerson].includes(`Color${i + 1}`) ? 'border-blue-500' : 'border-gray-300'
                                 }`}
                             onClick={() => handleColorSelect(`Color${i + 1}`)}
                         />
                     ))}
                 </div>
-
-                <br />
-                <br />
                 <div className="text-center mt-4">
                     <button onClick={saveSelectedColors} type="button" className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded">
                         Guardar Colores
--- src/components/OtraPagina.jsx
+++ src/components/OtraPagina.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const OtraPagina = () => {
-    // Estado para almacenar los datos del localStorage
     const [selectedColors, setSelectedColors] = useState({});
 
-    // Lógica para obtener los datos guardados en el localStorage
     useEffect(() => {
         const colorsFromLocalStorage = JSON.parse(localStorage.getItem('selectedColors'));
         if (colorsFromLocalStorage) {
@@ -13,7 +11,6 @@ const OtraPagina = () => {
         }
     }, []);
 
-    // Función para limpiar los campos y el localStorage
     const handleClear = () => {
         setSelectedColors({});
         localStorage.removeItem('selectedColors');
@@ -24,23 +21,27 @@ const OtraPagina = () => {
             <div className="text-center mt-4">
                 <img src="../assets/Logo.jpeg" alt="Logo" className="w-50 h-32 mx-auto mb-4" />
             </div>
-            <h1 className="text-3xl font-bold mb-4">Listado de Colores:</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-                {Object.keys(selectedColors).map(person => (
-                    <div key={person} className="bg-gray-200 p-4 rounded-lg">
-                        <h2 className="text-xl font-bold mb-2">{person}</h2>
-                        <div className="flex flex-wrap">
-                            {selectedColors[person].map((color, index) => (
-                                <div key={index} className="px-4 py-4 bg-indigo-300 text-black-900 rounded-full m-1">
-                                    {color}
+            <h1 className="text-3xl font-bold mb-4">Listado de Colores por Mesa:</h1>
+            {Object.keys(selectedColors).map(table => (
+                <div key={table}>
+                    <h2 className="text-2xl font-bold mb-2">{table}</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+                        {Object.keys(selectedColors[table]).map(person => (
+                            <div key={person} className="bg-gray-200 p-4 rounded-lg">
+                                <h3 className="text-xl font-bold mb-2">{person}</h3>
+                                <div className="flex flex-wrap">
+                                    {selectedColors[table][person].map((color, index) => (
+                                        <div key={index} className="px-4 py-4 bg-indigo-300 text-black-900 rounded-full m-1">
+                                            {color}
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div className=" text-center mt-4 mb-4">
-                {/* Botón para regresar a la pantalla principal */}
+                </div>
+            ))}
+            <div className="text-center mt-4 mb-4">
                 <Link
                     to="/"
                     onClick={handleClear}
