92bd9f20d7915a6d88031c83fc6d20e7d132bd17
Karen Cortes <karencortes9828@gmail.com>
2024-03-14 00:07:17 -0500

    feat: Details of installation and implementation of the background

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Asegúrate de importar 'Route' también
 import Colores from './components/Colores';
 import OtraPagina from './components/OtraPagina';
+import ParticleBackground from './components/Particle'
 
 const App = () => {
     return (
+
         <Router>
+            <ParticleBackground />
             <Routes>
                 <Route path="/" element={<Colores />} /> {/* Utiliza 'element' en lugar de 'component' */}
                 <Route path="/OtraPagina" element={<OtraPagina />} /> {/* Utiliza 'element' en lugar de 'component' */}
